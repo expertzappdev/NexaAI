0cb28d686248f99c08bccef4bcce38a9d6c0cdfe
sonwani-dev <sonwani.dev22@gmail.com>
2026-07-15 15:43:08 +0530

    feat/temporary-chat

--- Services/ConversationService.cs
+++ Services/ConversationService.cs
@@ -431,17 +431,6 @@ namespace AIChatBot.Services
                 searchResults = await _tavilyService.SearchAsync(content, cancellationToken);
             }
 
-            // Save User Message in Db first
-            var userMessage = new Message
-            {
-                ConversationId = conversationId,
-                Role = "user",
-                Content = content,
-                CreatedAt = DateTime.UtcNow
-            };
-            await _messageRepository.AddAsync(userMessage, cancellationToken);
-            await _messageRepository.SaveChangesAsync(cancellationToken);
-
             // 3. Build Groq Chat History
             var chatHistory = new List<GroqMessage>();
             
